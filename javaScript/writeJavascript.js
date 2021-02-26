e65bde41914a8de378bcfc0f7083f102c7db8b3d
golu linux <rohit343432@gmail.com>
2021-02-26 15:50:09 +0530

    javascript codes

--- javaScript/writeJavascript.js
+++ javaScript/writeJavascript.js
@@ -1,1 +1,35 @@
-document.write("hey how are you");
+// var name = rohit
+// var lastname = patel
+// var temp = `${name} "is a good boy" ${lastname}` // var string = "this";
+// var string = 'thi"s';
+// var name = 'Harry';
+// var channel = 'CodeWithHarry';
+// var message = 'Harry is a good boy';
+// var temp = `${name} is a 'nice' person "and" he has a channel called ${channel}`;
+// console.log(temp);
+// var len = name.length;
+// console.log(`Length of name is ${len}`)
+
+// console.log("Hello \\nworld\nHarry\tand");
+// var y = new String("this");
+// console.log(y);
+// document.getElementById('content').innerHTML = '<h3>this is an h3 heading</h3>'
+// document.getElementById('name').innerHTML = '<h1>rohit patel </h1>'
+
+
+// //logical operator
+// var name = "rohit";
+// var gender = "male";
+// if (gender == "male") {
+//     console.log("hello mr." + name);
+// } else {
+//     console.log("hello miss." +
+//         name);
+// }
+
+var marks = 30;
+if (marks >= 70 && marks <= 100) {
+    console.log("yes you are in merit");
+} else(
+    console.log("you are not in merit list");
+)
