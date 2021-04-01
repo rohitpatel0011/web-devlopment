1638ea7763a7e310539896b5136c04a3bfb7c84a
golu linux <rohit343432@gmail.com>
2021-04-01 23:23:08 +0530

    javascript

--- javaScript/Arrays.js
+++ javaScript/Arrays.js
@@ -3,3 +3,22 @@ console.log(a);
 let arr = ["39", 40, "golu", 65] //index of these arry is [0] , [1], [2], [3] 
 console.log(a[2]); //2 is a index of golu
 console.log(arr[2]); //1 is a index of 20
+
+//--------------------------------
+var ary = [10, 29, 39, 49, 38];
+for (var i = 0; i <= 4; i++) { //i initialization is zero because index of arry starts with 0 and with 4
+    console.log(ary[i]);
+}
+
+//------ADDING ARRAY IN INTIGERS VALUE-------
+let numbers = [
+    10, 50, 29, 60, 48,
+]
+let sum = 0;
+for (let n = 0; n <= 4; n++) {
+    console.log(numbers[n]);
+    sum = sum + numbers[n];
+}
+console.log("the total sum of all array is " + sum);
+
+//----------------------------
--- javaScript/Multidimensional-Arrays.js
+++ javaScript/Multidimensional-Arrays.js
@@ -1,2 +1,29 @@
-var names = ["rohit", "aman ", "nikku", "deepak"]
-console.log(names[3]);
+/* one arry in multiple array is 
+multidimensional array */
+//----------------------------------------------
+
+// var names = [
+//     ["golu", 34, "student", "B.C.A"],
+//     ["subham", 24, "student", "B.TCH"],
+//     ["aman", 31, "student", "B.C.A"],
+//     ["rahul", 43, "student", "B.C.A"]
+
+// ];
+// console.log(names[3]); //3 is a index of "rahul"colum 
+// console.log(names[0][1]); //0 is index of golu(inner row) and 1 is index of 34(outer colum)
+//----------------------------------
+// let ary = [
+//     ["rohit", 20, "male", "bca"],
+//     ["sapna", 19, "female", "b.com"],
+//     ["aman", 21, "male", "mca"]
+
+// ];
+// console.log(ary[1]);
+// for (let a = 0; a <= 2; a++) {
+//     console.log(ary[a]);
+// }
+// for (let a = 0; a <= 2; a++) {
+//     for (let b = 0; b <= 3; b++) {
+//         console.log(ary[a][b])
+//     }
+// }
