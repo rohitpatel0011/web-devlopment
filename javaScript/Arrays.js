a8a4b8b77af558d53d7bc3b76f577a2f6132d591
golu linux <rohit343432@gmail.com>
2021-04-03 14:05:03 +0530

    javaScript

--- javaScript/Arrays.js
+++ javaScript/Arrays.js
@@ -3,7 +3,6 @@ console.log(a);
 let arr = ["39", 40, "golu", 65] //index of these arry is ([0]is index of "39") , [1], [2], [3] 
 console.log(a[2]); //2 is a index of golu
 console.log(arr[2]); //1 is a index of 20
-<<<<<<< HEAD
 
 //--------------------------------
 var ary = [10, 29, 39, 49, 38];
@@ -11,7 +10,7 @@ for (var i = 0; i <= 4; i++) { //i initialization is zero because index of arry 
     console.log(ary[i]);
 }
 
-//------ADDING ARRAY IN INTIGERS VALUE-------
+//------ADDING ARRAY IN INTEGERS VALUE-------
 let numbers = [
     10, 50, 29, 60, 48,
 ]
@@ -22,6 +21,21 @@ for (let n = 0; n <= 4; n++) {
 }
 console.log("the total sum of all array is " + sum);
 
-//----------------------------
-=======
->>>>>>> fbf2303baac265083987d6956d886eaedb2c2cb3
+//--------------------Array new method--------
+var a = new Array(10, 20, 30, 40, 50, 60);
+console.log(a);
+//---- -- -- -- --
+var arry = new Array(); //creating blank arrays to fill unlimited value 
+var ary = new Array(4); //(4); fix the values of array 
+ary[0] = 20; //fill value using index of array
+ary[2] = "rohit";
+ary[3] = "subham";
+// for (var a = 0; a <= 3; a++) {
+//     console.log(ary[a]);
+
+// }
+ary[1] = "mohit";
+ary[4] = "rahul";
+ary[5] = "naman";
+
+console.log(ary);
