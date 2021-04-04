2178a9ec9895093074aea825eafcc9f296231135
golu linux <rohit343432@gmail.com>
2021-04-04 23:02:06 +0530

    javascript

---
+++ javaScript/arry-modify_delete.js
@@ -0,0 +1,12 @@
+//modifiy arrays in javascript
+var a = [
+    ["Rohit", 24, "male", "BCA "],
+    ["rakesh", 31, "male", "General"],
+    ["raushan", 17, "male", "javascript"]
+]
+
+// console.log(a);
+// a[0] = "Rahul"; //modify name of index [0]zero 
+// console.log(a);
+a[0][2] = "female";
+console.log(a);
