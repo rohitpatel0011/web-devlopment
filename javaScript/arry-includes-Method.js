7ab0e434bf777d4e47dcd7c449c882852c679f94
golu linux <rohit343432@gmail.com>
2021-04-09 11:28:57 +0530

    javascript

---
+++ javaScript/arry-includes-Method.js
@@ -0,0 +1,9 @@
+//----------------arrays include method-----
+var i = ["mango", "guava", "mango", "banana", "mango"];
+console.log(i);
+let j = i.includes("apple"); //include method checking the value in string ex
+console.log(j);
+let k = i.includes("mango");
+console.log(k);
+let l = i.indexOf("mango", 1);
+console.log(i);
