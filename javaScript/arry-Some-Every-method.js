358596de8a7d36d5e4585b9621b7db6b0d143a45
golu linux <rohit343432@gmail.com>
2021-04-10 13:18:08 +0530

    javaScript

--- javaScript/arry-Some-Every-method.js
+++ javaScript/arry-Some-Every-method.js
@@ -1,27 +1,32 @@
 //-------Arrays some method--------
-let ages = [12, 24, 35, 18, 20];
+let ages = [22, 24, 35, 127, 20];
 let b = ages.some(checkAdult);
 /*some method make with function
  to return in test*/
 
-function checkAdult(age) {
-    return age <= 18; /*or (return x <= 18;)test value in an arrays . if any one condition is true then output is true*/
+function checkAdult(age_) {
+    return age_ >= 200;
+    /*or (return x <= 18;)test value in an arrays
+     check every condition .
+       if any one condition is true then output is true*/
 }
 console.log(b);
 
 //----------Arrys Every Method--------
 let ages_ = [12, 24, 35, 18, 20];
-let i = ages_.Everysome(checkAdult2);
+let i = ages_.every(checkAdult2);
 
 function checkAdult2(age1) {
-    return age1 <= 10; /*if all condition is true then output is true*/
+    return age1 <= 10;
+    /*if all condition is 
+       true then output is true*/
 }
 console.log(i);
-let age = 29;
+
 
 //-------------function arrays-----------------
-function adding(a, b) {
-    var x = a + b;
+function adding(m, n) {
+    var x = m + n;
     return x;
 }
 
--- javaScript/arry-find-and-findIndex.js
+++ javaScript/arry-find-and-findIndex.js
@@ -5,7 +5,9 @@ let b = a.find(adultAge);
 
 function adultAge(age) {
     return age >= 18;
-    /*find() method return the value of the first element(not all element checking) in an arrays that pass a test*/
+    /*find() method return the value of 
+    the each  element( if one condition is true then pass the test and print that value)
+     in an arrays that pass a test*/
 }
 console.log(b);
 
@@ -13,6 +15,7 @@ console.log(b);
 //------Arrays find-index------
 let x = [12, 24, 35, 18, 20];
 let y = a.findIndex(adultAge_);
+/*( if one condition is true then pass the test and print that value)*/
 
 function adultAge_(age) {
     return age >= 18;
