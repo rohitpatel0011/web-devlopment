a47a14de703933efc07804107621626b645616ec
golu linux <rohit343432@gmail.com>
2021-04-10 12:53:03 +0530

    javascript

--- javaScript/arry-Some-Every-method.js
+++ javaScript/arry-Some-Every-method.js
@@ -11,9 +11,19 @@ console.log(b);
 
 //----------Arrys Every Method--------
 let ages_ = [12, 24, 35, 18, 20];
-let i = ages_.some(checkAdult2);
+let i = ages_.Everysome(checkAdult2);
 
 function checkAdult2(age1) {
     return age1 <= 10; /*if all condition is true then output is true*/
 }
 console.log(i);
+let age = 29;
+
+//-------------function arrays-----------------
+function adding(a, b) {
+    var x = a + b;
+    return x;
+}
+
+var add = adding(23, 45);
+console.log(add);
--- javaScript/arry-find-and-findIndex.js
+++ javaScript/arry-find-and-findIndex.js
@@ -1,7 +1,20 @@
+//-------Arrays find method-----------
+
 let a = [12, 24, 35, 18, 20];
 let b = a.find(adultAge);
 
 function adultAge(age) {
     return age >= 18;
+    /*find() method return the value of the first element(not all element checking) in an arrays that pass a test*/
 }
 console.log(b);
+
+
+//------Arrays find-index------
+let x = [12, 24, 35, 18, 20];
+let y = a.findIndex(adultAge_);
+
+function adultAge_(age) {
+    return age >= 18;
+}
+console.log(y);
