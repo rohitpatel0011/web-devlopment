e19ccb022fbc9c153f77d77236c5efbc088e9f32
golu linux <rohit343432@gmail.com>
2021-04-10 15:10:41 +0530

    javaScript

---
+++ javaScript/arry-TO-string-fill-method.js
@@ -0,0 +1,10 @@
+let a = ["rahul", "rohit", "aman", "subham"];
+let b = a.toString();
+/*The toString() 
+method convert 
+an array into string and return 
+the result*/
+let c = a.valueOf();
+//valueOf return value of elements in arrays 
+console.log(b);
+console.log(a);
