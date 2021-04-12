46fa1ae1b1993b69ec15e38546a5d93621fe0d7d
golu linux <rohit343432@gmail.com>
2021-04-12 17:39:17 +0530

    Objects In JavaScprit

--- javaScript/Object.js
+++ javaScript/Object.js
@@ -1,30 +1,36 @@
-// let employee = {
-//         names: "Rohit",
-//         age: 20,
-//         gender: "male",
-//         email: "abc@gmail.com",
-//         country: "india"
-//     }
-//     //The values are written as name : value pairs (name and value separated by a colon).
-// console.log(employee); //print the all object 
-// console.log(employee.gender); //print gender properties of the value
-// console.log(employee.country);
+//Objects in javascript------
+
+let employee = {
+        names: "Rohit",
+        age: 20,
+        gender: "male",
+        email: "abc@gmail.com",
+        country: "india"
+    }
+    //The values are written as name : value pairs (name and value separated by a colon).
+console.log(employee); //print the all object 
+console.log(employee.gender); //print gender properties of the value
+console.log(employee.country);
 
 
-// //-------Replace Arrays in Object -------
-// let employee_1 = {
-//     names: "Rohit",
-//     age: 20,
-//     gender: "male",
-//     language: ["python", "java", "php", "javascript"], //replacing arrays in object
-//     email: "abc@gmail.com",
-//     country: "india"
-// }
-// console.log(employee_1);
-// console.log(employee_1.language[0]); //o 0 is index of value
 
 
-// //--------------------in object function (methods)-----
+//-------Replace Arrays in Object -------
+let employee_1 = {
+    names: "Rohit",
+    age: 20,
+    gender: "male",
+    language: ["python", "java", "php", "javascript"], //replacing arrays in object
+    email: "abc@gmail.com",
+    country: "india"
+}
+console.log(employee_1);
+console.log(employee_1.language[0]); //o 0 is index of value
+
+
+
+
+//--------------------in object function (methods)-----
 let employee_2 = {
     names: "Golu",
     age: 20,
@@ -38,9 +44,10 @@ let employee_2 = {
 }
 console.log(employee_2.salary());
 
-//-------------
 
-// //------------------this method in object-----
+
+
+//------------------this method in object-----
 let employee_3 = {
     names: "Golu",
     age: 20,
@@ -48,6 +55,7 @@ let employee_3 = {
     language: ["python", "java", "php", "javascript"], //replacing arrays in object
     email: "abc@gmail.com",
     country: "india",
+    //-----this is object method---
     salary: function() { //making function method in objects and return value
         return 30000;
     },
@@ -56,18 +64,47 @@ let employee_3 = {
         /*this means target employee_3 and 
                this.name means employee_3 in name and age*
                (variable properties of value)*/
+        //----------------------
     },
 }
 console.log(employee_3.salary());
 console.log(employee_3.full_name());
 
 
-// function Book(type, author) {
-//     this.type = type;
-//     this.author = author;
-//     this.all = function() {
-//         return this.type + " written by " + this.author;
-//     }
-// }
-// var book = new Book("computer_science", " Rohit Patel");
-// console.log(book.all());
+
+
+//another example of this. method------------------------
+function Book(type, author) {
+    this.type = type;
+    this.author = author;
+    this.all = function() {
+        return this.type + " written by " + this.author;
+    }
+}
+var book = new Book("computer_science", " Rohit Patel");
+console.log(book.all());
+
+
+
+
+
+//---making object in object----------
+let employee_4 = {
+    names: "Golu",
+    age: 20,
+    gender: "male",
+    language: ["python", "java", "php", "javascript"], //replacing arrays in object
+    email: "abc@gmail.com",
+    country: "india",
+    salary: function() {
+        return 50000;
+    },
+    living: { //object in object
+        "city": "patna",
+        "state": "bihar",
+        "country": "india"
+
+    },
+}
+console.log(employee_4.salary());
+console.log(employee_4.living.city); //printing object in objects
