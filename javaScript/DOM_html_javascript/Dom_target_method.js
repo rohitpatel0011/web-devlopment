2c2fb52263abf3f03f6e30b3348749d27f5f4503
golu linux <rohit343432@gmail.com>
2021-04-26 04:37:18 +0530

    javascript

--- javaScript/DOM_html_javascript/Dom_target_method.js
+++ javaScript/DOM_html_javascript/Dom_target_method.js
@@ -1,2 +1,61 @@
-var a = string.fromCharCode(105);
-document.write(a);
+//*---------Target particular element in dom----------
+
+//-------------------------------
+var elements = document.body;
+console.log(elements);
+
+
+
+
+//-----------------
+var element = document.links;
+var elem = document.links[2];
+//2 is index of link
+//.link return length of links in arrays
+console.log(element);
+console.log(elem);
+
+
+//---------------
+var ele = document.getElementById("header");
+//return id name of header all elements 
+console.log(ele);
+
+
+//------------------------------------------------
+var el = document.getElementsByClassName("perentdiv");
+console.log(el);
+console.log(el.length);
+
+
+
+//-----------------------------
+var elemen = document.getElementsByTagName("input"); //("input")[5];//this or
+// console.log(elemen[5]);//this both are same
+console.log(elemen);
+
+
+
+
+//*-----Dom Get and Set method--------------------
+
+
+//*------.innerText---------
+//*-----.innerHTML----------
+//*-----.getAttribute-------
+//*-----.getAttributeNode---
+//*-----Attributes----------
+
+//----innerText-----
+var ement = document.getElementById("paragraph").innerText;
+console.log(ement);
+
+
+
+//---innerHTML--------.
+var e = document.getElementById("paragraph").innerHTML;
+console.log(e);
+
+
+
+//----getAttribute-----
